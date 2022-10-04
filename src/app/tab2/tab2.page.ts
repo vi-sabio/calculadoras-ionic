9b0f5ad49a9eda2259f416fa9af8edff13d52f5d
Aline Otília <aos_santos@hotmail.com>
2022-10-03 22:25:11 -0300

    adicionando funções

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,44 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  operacao = '';
+  resultado = '';
+  ultimoValor = '';
+
   constructor() {}
+
+  adicionarValor(valor: string){
+
+    if(this.operacao !== ''){
+      this.ultimoValor = this.operacao[this.operacao.length-1];
+    }
+
+    if(valor === '/' || valor === '*' || valor === '-'|| valor === '+' || valor === '.'){
+      if( this.operacao === '' ||this.ultimoValor === '/' || this.ultimoValor === '*'||
+      this.ultimoValor === '-'|| this.ultimoValor === '+' || this.ultimoValor === '.' ){
+        return;
+      }
+    }
+    this.operacao += valor;
+  }
+
+  limparOperacao(){
+    this.operacao = '';
+  }
+
+  limparMemoria(){
+    this.resultado = '';
+    this.operacao = '';
+  }
+
+  apagarCaracter(){
+    if(this.operacao.length > 0){
+      this.operacao = this.operacao.substring(0,this.operacao.length-1);
+    }
+  }
+
+  calcularResultado(){
+
+  }
 
 }
